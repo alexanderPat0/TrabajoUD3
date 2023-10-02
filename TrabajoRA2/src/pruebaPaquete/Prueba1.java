de15572e6d523ff9366ad180ea217d3a47e74f29
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2023-10-02 14:16:30 +0200

    Prueba jdk21

--- TrabajoRA2/src/pruebaPaquete/Prueba1.java
+++ TrabajoRA2/src/pruebaPaquete/Prueba1.java
@@ -7,6 +7,7 @@ public class Prueba1 {
 		System.out.println("Hola mundo");
 		System.out.println("Prueba commit fernando");
 		System.out.println("Prueba commit jj");
+		System.out.println("AAAAAAAA");
 	}
 
 }
