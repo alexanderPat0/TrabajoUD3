c9d6f2de3ff152c2b2f30639e4b7e3928dae2077
Fernando Villalba <fervillalbita@gmail.com>
2023-10-02 13:23:10 +0200

    commit fernando comprobando que funcione

--- TrabajoRA2/src/pruebaPaquete/Prueba1.java
+++ TrabajoRA2/src/pruebaPaquete/Prueba1.java
@@ -5,6 +5,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Prueba commit fernando");
 	}
 
 }
