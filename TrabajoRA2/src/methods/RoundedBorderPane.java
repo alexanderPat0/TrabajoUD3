15a7ea1e7f1c0be3b417812ccbb49df8a7206ca9
Alejandro Rodriguez <rf05alejandro@gmail.com>
2023-10-03 10:09:27 +0200

    Ajustes roundedBorderPane

--- TrabajoRA2/src/methods/RoundedBorderPane.java
+++ TrabajoRA2/src/methods/RoundedBorderPane.java
@@ -9,9 +9,9 @@ import java.awt.geom.RoundRectangle2D;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class TestPane extends JPanel {
+public class RoundedBorderPane extends JPanel {
 
-    public TestPane() {
+    public RoundedBorderPane() {
     }
 
     @Override
@@ -23,9 +23,7 @@ public class TestPane extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g.create();
-        
-        int size = Math.min(getWidth(), getHeight()) / 2;
-        
+                
         RoundRectangle2D border = new RoundRectangle2D.Double(0, 0, 210, 200, 20, 20);
         ImageUtilities.applyQualityRenderingHints(g2d);
         g2d.drawImage(ImageUtilities.applyShadow(border, 2, getBackground(), Color.BLACK, 0.25f), 5, 5, this);
--- TrabajoRA2/src/views/Login.java
+++ TrabajoRA2/src/views/Login.java
@@ -2,20 +2,28 @@ package views;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
-import methods.TestPane;
+import methods.ImageUtilities;
+import methods.RoundedBorderPane;
+
 import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class Login extends JFrame {
@@ -28,50 +36,54 @@ public class Login extends JFrame {
 		
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 301, 303);
+		setBounds(100, 100, 301, 328);
 		setLocationRelativeTo(null);
-		setVisible(true);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNoAcc = new JLabel("New account?");
-		lblNoAcc.setBounds(65, 230, 83, 14);
+		JLabel lblNoAcc = new JLabel("You haven't registered yet?");
+		lblNoAcc.setBounds(68, 230, 169, 14);
 		contentPane.add(lblNoAcc);
 
 		JLabel lblRegister = new JLabel("Register");
-		lblRegister.setBounds(160, 230, 59, 14);
+		lblRegister.setBounds(112, 255, 59, 14);
 		lblRegister.setForeground(Color.BLUE.darker());
 		lblRegister.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		contentPane.add(lblRegister);
 		
-		JPanel panel = new TestPane();
+		JPanel panel = new RoundedBorderPane();
 		panel.setBounds(31, 11, 223, 208);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(75, 42, 83, 14);
+		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblUsername.setBounds(38, 22, 105, 24);
 		panel.add(lblUsername);
 				
 		txtUsername = new JTextField();
-		txtUsername.setBounds(38, 64, 139, 20);
+		txtUsername.setBounds(38, 48, 139, 28);
 		panel.add(txtUsername);
 		txtUsername.setColumns(10);
 						
 		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(75, 95, 83, 14);
+		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPassword.setBounds(38, 87, 105, 24);
 		panel.add(lblPassword);
 								
 		passwordField = new JPasswordField();
-		passwordField.setBounds(38, 117, 139, 20);
+		passwordField.setBounds(38, 114, 139, 28);
 		panel.add(passwordField);
 		
-		JButton btn_ = new JButton("New button");
-		btn_.setBounds(0, 0, 89, 23);
-		panel.add(btn_);
+		JButton btnLogin = new JButton("Log in");
+		btnLogin.setBackground(Color.BLUE);
+		btnLogin.setForeground(new Color(255, 255, 255));
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnLogin.setBounds(38, 153, 139, 28);
+		panel.add(btnLogin);
 
 		lblRegister.addMouseListener(new MouseAdapter() {
 			@Override
@@ -80,6 +92,7 @@ public class Login extends JFrame {
 				new Register();
 			}
 		});
+		setVisible(true);
 
 	}
 }
