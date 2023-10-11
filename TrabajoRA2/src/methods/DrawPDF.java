f5949693254490c40594885516cc50c6d56b8639
alexanderPat0 <alex0rodriguez7fenix@gmail.com>
2023-10-11 09:16:12 +0200

    Añadidas librerias y continuado con pdf

--- TrabajoRA2/src/methods/DrawPDF.java
+++ TrabajoRA2/src/methods/DrawPDF.java
@@ -13,7 +13,11 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
+import be.quodlibet.boxable.BaseTable;
+import be.quodlibet.boxable.Cell;
+import be.quodlibet.boxable.Row;
 import models.Transaction;
+import services.Conexion;
 import test.Test;
 
 public class DrawPDF {
@@ -53,32 +57,45 @@ public class DrawPDF {
 		
 		transactions.setFont(PDType1Font.HELVETICA_BOLD, 15);
 		transactions.beginText();
-		transactions.newLineAtOffset(50, 635);
-		transactions.showText("Product:");
-		transactions.newLineAtOffset(90, 635);
-		transactions.showText("PPU:");
-		transactions.newLineAtOffset(130, 635);
-		transactions.showText("Quantity:");
-		transactions.newLineAtOffset(170, 635);
-		transactions.showText("Price:");
-		
-		
-		
-		
-		transactions.endText();
 
-		transactions.setFont(PDType1Font.HELVETICA, 12);
-		transactions.beginText();
-		transactions.newLineAtOffset(50, 650);
-		
-		for (Transaction t : Test.transactionList) {
-			
+	    float margin = 50;
+	    float yStartNewPage = page.getMediaBox().getHeight() - (2 * margin);
+	    float tableWidth = page.getMediaBox().getWidth() - (2 * margin);
+
+	    boolean drawContent = true;
+	    float yStart = yStartNewPage;
+	    float bottomMargin = 70;
+	    float yPosition = 550;
+
+	    BaseTable table = new BaseTable(yPosition, yStartNewPage, bottomMargin, tableWidth, margin, pdf, page, true, drawContent);
+
+
+	    Row<PDPage> headerRow = table.createRow(15f);
+	    Cell<PDPage> cell;
+	    cell = headerRow.createCell(25, "Product");
+	    cell = headerRow.createCell(25, "Unit price");
+	    cell = headerRow.createCell(25, "Quantity");
+	    cell = headerRow.createCell(25, "Total price");
+	    
+	    table.addHeaderRow(headerRow);
+
+	    float finalPrice = 0;
+	    for (Transaction t : Test.transactionList) {
+	    	float totalPrice = Test.product.getProduct(Conexion.obtain(), t.getId_prod()).getPrice() * t.getAmount() ;
+	    	finalPrice += totalPrice;
+	    	Row<PDPage> row = table.createRow(12);
+	    	cell = row.createCell(25, Test.product.getProduct(Conexion.obtain(), t.getId_prod()).getName() );
+	    	cell = row.createCell(25, String.valueOf( Test.product.getProduct(Conexion.obtain(), t.getId_prod()).getPrice()));
+	    	cell = row.createCell(25, String.valueOf(t.getAmount()));
+	    	cell = row.createCell(25, String.valueOf(totalPrice));
 		}
-		
+	    
+	    table.draw();
 
+	    pdf.addPage(page);
 		transactions.endText();
 		transactions.close();
-
+		
 		JFrame parentFrame = new JFrame();
 
 		JFileChooser fileChooser = new JFileChooser();
